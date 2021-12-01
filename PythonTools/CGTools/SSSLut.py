9d1a8870f59af86fbb86b0825d8c880bd8befad8
HkingAuditore <178854663@qq.com>
2021-12-01 23:43:22 +0800

    高斯

--- PythonTools/CGTools/SSSLut.py
+++ PythonTools/CGTools/SSSLut.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import numpy as np
-img = Image.new('RGB', (512, 512))
+import SumOfGaussians
+size = 128
+img = Image.new('RGB', (size, size))
 
 img_array = np.array(img)#把图像转成数组格式img = np.asarray(image)
 shape = img_array.shape
@@ -8,10 +10,18 @@ shape = img_array.shape
 height = shape[0]
 width = shape[1]
 dst = np.zeros((height,width,3))
+
+
 for h in range(0,height):
     for w in range (0,width):
-        img_array[h, w] = (255,0,0)
-        dst[h,w] = img_array[h,w]
+        d = np.linalg.norm([h/size - .5,w/size - .5])
+        # print(d)
+        reflectance = SumOfGaussians.R(d, SumOfGaussians.light_weights_tuples, SumOfGaussians.v)
+        RR = SumOfGaussians.saturate(reflectance[0])
+        RG = SumOfGaussians.saturate(reflectance[1])
+        RB = SumOfGaussians.saturate(reflectance[2])
+        dst[h,w] = [RR * 255,RG* 255,RB* 255]
+
 img2 = Image.fromarray(np.uint8(dst))
 img2.show(img2)
 img2.save("3.png","png")
--- PythonTools/CGTools/SumOfGaussians.py
+++ PythonTools/CGTools/SumOfGaussians.py
@@ -1,42 +1,66 @@
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 from matplotlib import pyplot as plt
 
+v = [.0064, .0484, .197, .567, 1.99, 7.41]
+light_weights_tuples = [
+    (.233,.455,.649),
+    (.1,.336,.344),
+    (.118,.198,.0),
+    (.113,.007,.007),
+    (.358,.004,.0),
+    (.078,.0,.0),
+    ]
+
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
-plt.axis([0,2.5,0,1])
+# plt.axis([0, 2.5, 0, 1])
+
 
 def gaussian(v, r):
     return (1 / (2 * np.pi * v)) * np.exp(-np.power(r, 2) / (2 * v))
 
-v = [.0064,.0484,.197,.567,1.99,7.41]
+def saturate(v):
+    return max(0,min(v,1))
+
+def gaussian2(v, r):
+    return np.exp(-np.power(r, 2) / v)
+
+
+def R(d,l,vl):
+    s = [0,0,0]
+    for i,t in enumerate(l):
+        r,g,b = t
+        v =[r,g,b]
+        # v /= np.linalg.norm(v)
+        # print(v)
+        s[0] += v[0] * gaussian2(vl[i],d)
+        s[1] += v[1] * gaussian2(vl[i],d)
+        s[2] += v[2] * gaussian2(vl[i],d)
+    return s
+
+
+
+
+
+
 r = np.arange(0, 2.5, 0.01)
-R = lambda x, l: l[0] * gaussian(.0064, x) \
-                 + l[1] * gaussian(.0484, x) \
-                 + l[2] * gaussian(.187, x) \
-                 + l[3] * gaussian(.567, x) \
-                 + l[4] * gaussian(1.99, x) \
-                 + l[5] * gaussian(7.41, x)
+
 # R2 = lambda x, l: l[0] * gaussian(.036, x) \
 #                  + l[1] * gaussian(.14, x) \
 #                  + l[2] * gaussian(.91, x) \
 #                  + l[3] * gaussian(7.0, x)
-RR = R(r, [.233, .1, .118, .113, .358, .078]) * r
-RG = R(r, [.455, .336, .198, .007, .004, 0]) * r
-RB = R(r, [.649, .344, .0, .007, 0, 0]) * r
 
-
-
+Rr = R(r,light_weights_tuples,v)
 
 # RF = R2(r, [.07,.18,.21,.29]) * r
-print(R)
+# print(R)
 plt.xlabel('r')
 plt.ylabel('rR(r)')
 plt.title("散射拟合")
-plt.plot(r, RR, color='red')
-plt.plot(r, RG, color='green')
-plt.plot(r, RB, color='blue')
+plt.plot(r,Rr[0] * r, color='red')
+plt.plot(r,Rr[1] * r, color='green')
+plt.plot(r,Rr[2] * r, color='blue')
 
 # plt.plot(r, 2 * np.pi * RF, color='black')
 plt.show()
