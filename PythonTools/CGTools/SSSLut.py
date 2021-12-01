b4e7346483556104e943001509e2c354aea4d27b
HkingAuditore <178854663@qq.com>
2021-12-02 01:47:33 +0800

    无法理解的结果...

--- PythonTools/CGTools/SSSLut.py
+++ PythonTools/CGTools/SSSLut.py
@@ -1,27 +1,100 @@
 from PIL import Image
 import numpy as np
+from multipledispatch import dispatch
+
 import SumOfGaussians
+
 size = 128
 img = Image.new('RGB', (size, size))
 
-img_array = np.array(img)#把图像转成数组格式img = np.asarray(image)
+
+def sample_light(angle, a=0, b=0):
+    return np.cos(angle + a) * np.cos(b)
+
+
+def integrate_up(theta, radius, accuracy=.1, use_sphere = False):
+    if use_sphere:
+        delta_a = np.math.pi * accuracy
+        delta_b = 2 * np.math.pi * accuracy
+        a = - .5 * np.math.pi
+        result = [0, 0, 0]
+        while a <= .5 * np.math.pi:
+            b = -np.math.pi
+            while b <= np.math.pi:
+                result += np.multiply(SumOfGaussians.saturate(sample_light(theta, a, b)), SumOfGaussians.R(a, b, radius))
+                b += delta_b
+            a += delta_a
+        return result
+    else:
+        delta_b = 2 * np.math.pi * accuracy
+        result = [0, 0, 0]
+        b = -np.math.pi
+        while b <= np.math.pi:
+            result += np.multiply(SumOfGaussians.saturate(sample_light(theta, b)), SumOfGaussians.R(b, radius))
+            b += delta_b
+        return result
+
+
+
+def integrate_bottom(radius, accuracy=.1, use_sphere = False):
+    if use_sphere:
+        delta_a = np.math.pi * accuracy
+        delta_b = 2 * np.math.pi * accuracy
+        a = - .5 * np.math.pi
+        result = [0, 0, 0]
+        while a <= .5 * np.math.pi:
+            b = -np.math.pi
+            while b <= np.math.pi:
+                r = SumOfGaussians.R(a, b, radius)
+                result[0] += r[0]
+                result[1] += r[1]
+                result[2] += r[2]
+                b += delta_b
+            a += delta_a
+        return result
+    else:
+        delta_b = 2 * np.math.pi * accuracy
+        result = [0, 0, 0]
+        b = -np.math.pi
+        while b <= np.math.pi:
+            r = SumOfGaussians.R(b, radius)
+            result[0] += r[0]
+            result[1] += r[1]
+            result[2] += r[2]
+            b += delta_b
+        return result
+
+
+
+def integrate(theta, thickness, accuracy=.1):
+    up = integrate_up(theta, thickness, accuracy)
+    bottom = integrate_bottom(thickness, accuracy)
+    return [up[0] / bottom[0], up[1] / bottom[1], up[2] / bottom[2]]
+
+
+
+
+img_array = np.array(img)  # 把图像转成数组格式img = np.asarray(image)
 shape = img_array.shape
 
 height = shape[0]
 width = shape[1]
-dst = np.zeros((height,width,3))
+dst = np.zeros((height, width, 3))
 
+for h in range(0, height):
+    for w in range(0, width):
+        uv = [w / width - .5, h / height - .5]
+        col = integrate(np.acos(uv[0]), uv[1],accuracy=.1)
 
-for h in range(0,height):
-    for w in range (0,width):
-        d = np.linalg.norm([h/size - .5,w/size - .5])
-        # print(d)
-        reflectance = SumOfGaussians.R(d, SumOfGaussians.light_weights_tuples, SumOfGaussians.v)
-        RR = SumOfGaussians.saturate(reflectance[0])
-        RG = SumOfGaussians.saturate(reflectance[1])
-        RB = SumOfGaussians.saturate(reflectance[2])
-        dst[h,w] = [RR * 255,RG* 255,RB* 255]
+        # print(col)
+        dst[w, h] = [col[0] * 255, col[1] * 255, col[2] * 255]
+        # print(dst[w, h])
+        # reflectance = SumOfGaussians.R(d, SumOfGaussians.light_weights_tuples, SumOfGaussians.v)
+        # RR = SumOfGaussians.saturate(reflectance[0])
+        # RG = SumOfGaussians.saturate(reflectance[1])
+        # RB = SumOfGaussians.saturate(reflectance[2])
+        # dst[h,w] = [RR * 255,RG* 255,RB* 255]
 
 img2 = Image.fromarray(np.uint8(dst))
 img2.show(img2)
-img2.save("3.png","png")
+img2.save("3.png", "png")
--- PythonTools/CGTools/SumOfGaussians.py
+++ PythonTools/CGTools/SumOfGaussians.py
@@ -1,47 +1,110 @@
+from numbers import Number
+
 import numpy as np
 
 from matplotlib import pyplot as plt
+from multipledispatch import dispatch
 
 v = [.0064, .0484, .197, .567, 1.99, 7.41]
 light_weights_tuples = [
-    (.233,.455,.649),
-    (.1,.336,.344),
-    (.118,.198,.0),
-    (.113,.007,.007),
-    (.358,.004,.0),
-    (.078,.0,.0),
-    ]
+    (.233, .455, .649),
+    (.1, .336, .344),
+    (.118, .198, .0),
+    (.113, .007, .007),
+    (.358, .004, .0),
+    (.078, .0, .0),
+]
 
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+
+
 # plt.axis([0, 2.5, 0, 1])
 
 
 def gaussian(v, r):
     return (1 / (2 * np.pi * v)) * np.exp(-np.power(r, 2) / (2 * v))
 
+
 def saturate(v):
-    return max(0,min(v,1))
+    return max(0, min(v, 1))
+
 
 def gaussian2(v, r):
     return np.exp(-np.power(r, 2) / v)
 
 
-def R(d,l,vl):
-    s = [0,0,0]
-    for i,t in enumerate(l):
-        r,g,b = t
-        v =[r,g,b]
+@dispatch(object)
+def R(d, l=light_weights_tuples, vl=v):
+    s = [0, 0, 0]
+    for i, t in enumerate(l):
+        r, g, b = t
+        v = [r, g, b]
         # v /= np.linalg.norm(v)
         # print(v)
-        s[0] += v[0] * gaussian2(vl[i],d)
-        s[1] += v[1] * gaussian2(vl[i],d)
-        s[2] += v[2] * gaussian2(vl[i],d)
+        s[0] += v[0] * gaussian2(vl[i], d)
+        s[1] += v[1] * gaussian2(vl[i], d)
+        s[2] += v[2] * gaussian2(vl[i], d)
     return s
 
 
+@dispatch(Number, Number, Number)
+def R(a, b, r):
+    d = r * np.sqrt(2 - 2 * np.cos(a) * np.cos(b))
+    return R(d)
 
+@dispatch(Number, Number)
+def R(a, r):
+    d = 2 * r *np.sin(.5 * a)
+    return R(d)
 
+@dispatch(Number, Number, Number, list, list)
+def R(a, b, r, l=light_weights_tuples, vl=v):
+    d = r * np.sqrt(2 - 2 * np.cos(a) * np.cos(b))
+    return R(d, l, vl)
+
+def G1(Neg_r_2, v):
+    return np.exp(Neg_r_2)
+
+def G2(Neg_r_2, v):
+    v2 = 2.0 * v
+    return 1.0/(v2 * np.math.pi) * np.exp(Neg_r_2/v2)
+
+def Cal(distance,G):
+    Neg_r_2 = -np.multiply(distance,distance)
+    rgb = [0,0,0]
+    g = [G(Neg_r_2 , 0.0064),
+         G(Neg_r_2 , 0.0484),
+         G(Neg_r_2 , 0.1870),
+         G(Neg_r_2 , 0.5670),
+         G(Neg_r_2 , 1.9900),
+         G(Neg_r_2 , 7.4100),
+         ]
+    rgb[0] += 0.233 *g[0] +\
+              0.100 *g[1] +\
+              0.118 *g[2] +\
+              0.113 *g[3] +\
+              0.358 *g[4] +\
+              0.078 *g[5]
+    rgb[1] += 0.455 *g[0] +\
+              0.336 *g[1] +\
+              0.198 *g[2] +\
+              0.007 *g[3] +\
+              0.004 *g[4] +\
+              0.000 *g[5]
+    rgb[2] += 0.649 *g[0] +\
+              0.344 *g[1] +\
+              0.000 *g[2] +\
+              0.007 *g[3] +\
+              0.000 *g[4] +\
+              0.000 *g[5]
+    # rgb += [0.233,0.455,0.649] * G(Neg_r_2 , 0.0064)+\
+    #        [0.100,0.336,0.344] * G(Neg_r_2 , 0.0484)+\
+    #        [0.118,0.198,0.000] * G(Neg_r_2 , 0.1870)+\
+    #        [0.113,0.007,0.007] * G(Neg_r_2 , 0.5670)+\
+    #        [0.358,0.004,0.000] * G(Neg_r_2 , 1.9900)+\
+    #        [0.078,0.000,0.000] * G(Neg_r_2 , 7.4100)
+    return rgb
 
 
 r = np.arange(0, 2.5, 0.01)
@@ -51,16 +114,16 @@ r = np.arange(0, 2.5, 0.01)
 #                  + l[2] * gaussian(.91, x) \
 #                  + l[3] * gaussian(7.0, x)
 
-Rr = R(r,light_weights_tuples,v)
+Rr = Cal(r,G2)
 
 # RF = R2(r, [.07,.18,.21,.29]) * r
-# print(R)
+print(R)
 plt.xlabel('r')
 plt.ylabel('rR(r)')
 plt.title("散射拟合")
-plt.plot(r,Rr[0] * r, color='red')
-plt.plot(r,Rr[1] * r, color='green')
-plt.plot(r,Rr[2] * r, color='blue')
+plt.plot(r, Rr[0] * r, color='red')
+plt.plot(r, Rr[1] * r, color='green')
+plt.plot(r, Rr[2] * r, color='blue')
 
 # plt.plot(r, 2 * np.pi * RF, color='black')
 plt.show()
