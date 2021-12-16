333adb36c4c116ca22d5f1b63ab9f7eb5ed8dee6
HkingAuditore <178854663@qq.com>
2021-12-16 23:34:14 +0800

    建筑文档

--- PythonTools/TechTreeGraph/Default.py
+++ PythonTools/TechTreeGraph/Default.py
@@ -21,14 +21,14 @@ g.add_node("军事训练", ["X>【民兵】", "+>【剑士】", "+>【长矛兵�
 g.add_node("战术", ["+>【弓箭手】"], 20, [10, 20],former_nodes=["军事训练"], node_type="MIL")
 g.add_node("马镫", ["+>【骑手】"], 20, [10, 20],former_nodes=["战术"], node_type="MIL")
 g.add_node("医药", ["+>【军医】"], 20, [10, 20], former_nodes=["战术"], node_type="MIL")
-g.add_node("军事工程", ["+>【拒马】","+>【堡垒】"], 30, [10, 20], former_nodes=["马镫"], node_type="MIL")
+g.add_node("军事工程", ["+>【拒马】"], 30, [10, 20], former_nodes=["马镫"], node_type="MIL")
 g.add_node("攻城器械", ["+>【冲车】","+>【投石机】"], 30, [10, 20], former_nodes=["军事工程"], node_type="MIL")
-g.add_node("火药", ["+>【火门枪手】"], 20, [10, 20], former_nodes=["攻城器械"], node_type="MIL")
+g.add_node("火药", ["+>【火枪手】"], 20, [10, 20], former_nodes=["攻城器械"], node_type="MIL")
 g.add_node("火炮", ["+>【火炮】"], 20, [10, 20], former_nodes=["火药"], node_type="MIL")
 
 
 g.add_node("军功制", ["提高步兵攻击力","提高步兵防御力","+>【兵营】"], 30, [10, 20], former_nodes=["军事训练"], node_type="MIL")
-g.add_node("复合弓", ["提高远程单位攻击力","提高射击距离","+>【靶场】"], 30, [10, 20], former_nodes=["战术"], node_type="MIL")
+g.add_node("复合弓", ["提高远程单位攻击力","提高射击距离","+>【靶场】","+>【驻防点】"], 30, [10, 20], former_nodes=["战术"], node_type="MIL")
 g.add_node("马匹育种", ["提高骑兵速度","降低骑兵价格","+>【马厩】"], 30, [10, 20], former_nodes=["马镫"], node_type="MIL")
 g.add_node("军事技术", ["提高攻城器械生产速度","降低攻城器械价格","+>【兵工厂】"], 30, [10, 20], former_nodes=["攻城器械"], node_type="MIL")
 
